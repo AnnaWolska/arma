dcf1180028aeefc3f8af7c6e5359cdf5a352d3bb
AnnaWolska <a_wl@wp.pl>
2023-03-07 13:29:13 +0100

    herok try 6

--- arma/settings.py
+++ arma/settings.py
@@ -370,12 +370,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT =  BASE_DIR / "static"
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    # BASE_DIR / 'static'
-    os.path.join(BASE_DIR, 'static'),
-]
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# STATIC_ROOT =  BASE_DIR / "static"
+# STATICFILES_DIRS = [
+#     # BASE_DIR / 'static'
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR / "images"
