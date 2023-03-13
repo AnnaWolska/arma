ff4c3732ab45ba9a8f35eee2a9aa018b3b9df4ff
AnnaWolska <a_wl@wp.pl>
2023-03-13 11:26:31 +0100

    working progress get rid dotenv

--- arma/settings.py
+++ arma/settings.py
@@ -15,8 +15,8 @@ import os
 # import dj_database_url
 
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 import cloudinary
 import cloudinary.uploader
