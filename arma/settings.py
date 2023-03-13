af1036b6ab15855a1a1de4e660478506aa2862f4
AnnaWolska <a_wl@wp.pl>
2023-03-13 13:11:31 +0100

    working progress static_url

--- arma/settings.py
+++ arma/settings.py
@@ -147,12 +147,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = "/static/"
 
-STATIC_URL = 'statics/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'statics'
 ]
 # LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # Default primary key field type
