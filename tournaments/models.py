740b9f842f5ce031545c51e04d31fac7e5f207b0
AnnaWolska <a_wl@wp.pl>
2023-04-03 10:30:23 +0200

    cloudinary try5 models

--- tournaments/models.py
+++ tournaments/models.py
@@ -28,7 +28,7 @@ class Tournament(models.Model):
     description = models.TextField(null=True, blank=True)
     organizers = models.ManyToManyField('Organizer', related_name="tournaments")
     # image = ImageField(upload_to="tournaments/logos/%Y/%m/%d/", blank=True, null=True)
-    image = CloudinaryField(upload_to="tournaments/logos/%Y/%m/%d/", blank=True, null=True)
+    image = CloudinaryField(blank=True, null=True)
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="tournaments_created")
     created = models.DateTimeField(auto_now=True, null=True)
     modified = models.DateTimeField(auto_now=True, null=True)
