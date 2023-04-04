41f8589f095e37df1f31d9a84b4fb9c0f69d2123
AnnaWolska <a_wl@wp.pl>
2023-04-04 11:54:33 +0200

    cloudinary SATICFILES_DIR no

--- arma/settings.py
+++ arma/settings.py
@@ -167,8 +167,8 @@ else:
 #     # BASE_DIR / 'static'
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
+# ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
